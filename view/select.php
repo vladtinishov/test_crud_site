99d05072a2e65ba1e6c3aa549dfedff772aeab87
vladtinishov <vladislav04022001@gmail.com>
2021-02-14 10:51:14 +0600

    редактировал страницу с записями

--- view/select.php
+++ view/select.php
@@ -1,10 +1,11 @@
 <div class="main">
 <?php foreach($select->select_all() as $key => $value):?>
     <div class="record">
+        Заголовок:
         <a href="/controllers/single.php?id=<?php echo $value['id'] ?>">
             <?php echo $value['title'] ?>
         </a>
-        <p><?php echo $value['content'] ?></p>
+        <p>Контент: <?php echo $value['content'] ?></p>
         <a href="../controllers/delete.php?id=<?php echo $value['id']?>">Delete</a>
         <a href="../controllers/update.php?id=<?php echo $value['id']?>">Update</a>
         <hr>
