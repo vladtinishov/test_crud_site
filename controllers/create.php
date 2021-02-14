676add1933664f0ea2cd194da60cf83bb561e7cb
vladtinishov <vladislav04022001@gmail.com>
2021-02-14 13:13:25 +0600

    поставил табы в условии контроллера create

--- controllers/create.php
+++ controllers/create.php
@@ -3,14 +3,14 @@
 require_once('../models/create.php');
 
 if(isset($_GET['title']) && isset($_GET['content'])){
-$cr = new Create();
-$data = array(
-    'title' => $_GET['title'],
-    'content' => $_GET['content'],
-);
-$sql = "INSERT INTO test(title, content) VALUES (:title, :content)";
-$cr->create($data, $sql);
-echo '<span style="background: green; color: white; padding: 10px; border-radius:10px">Создано</span>';
+    $cr = new Create();
+    $data = array(
+        'title' => $_GET['title'],
+        'content' => $_GET['content'],
+    );
+    $sql = "INSERT INTO test(title, content) VALUES (:title, :content)";
+    $cr->create($data, $sql);
+    echo '<span style="background: green; color: white; padding: 10px; border-radius:10px">Создано</span>';
 }
 ?>
 
