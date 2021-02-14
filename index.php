9af7f23ce6b1cd64f254404befe81616c92622ba
vladtinishov <vladislav04022001@gmail.com>
2021-02-14 13:57:50 +0600

    измененны стили в index странице

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
-<?php require_once('view/templates/header.php');?>
-    
-<div class="main">
-    <h1><a href="controllers/select.php">Главная</a> <a href="controllers/create.php">Создать</a></h1>
+<?php $title="Главная"; require_once('view/templates/header.php');?>
+
+<style>
+body{
+    margin: 0;
+    padding: 0;
+}
+.links{
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+}
+h1{
+    margin: 0;
+    padding: 0;
+}
+.links_inner{
+    padding: 50px;
+    border-radius: 10px;
+    background: white;
+}
+</style>
+<div class="links">
+    <div class="links_inner">
+        <h1><a href="controllers/select.php">Главная</a><br>
+        <a href="controllers/create.php">Создать</a></h1>
+    </div>
 </div>
 
 <?php require_once('view/templates/footer.php');?>
