5e567fc448bf83a1dac3d03cf74fc8ba6f32080d
vladtinishov <vladislav04022001@gmail.com>
2021-02-14 11:09:47 +0600

    Удалить поменял на Удалено

--- controllers/delete.php
+++ controllers/delete.php
@@ -4,7 +4,7 @@ require_once('../models/delete.php');
 $del = new Delete();
 $del->delete('test', 'id', $_GET['id']);
 
-echo '<span style="background: green; color: white; padding: 10px; border-radius:10px">Удалить</span>';
+echo '<span style="background: green; color: white; padding: 10px; border-radius:10px">Удалено</span>';
 
 $title = 'Добавить запись';
 require_once('../view/templates/header.php');
